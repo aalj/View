522f51973e066d984b46b515b931507937dd7492
aalj <dqliangjun@126.com>
2017-05-04 18:29:46 +0800

    增加一个圆形控件

--- MyView/app/src/main/java/com/myview/MainActivity.java
+++ MyView/app/src/main/java/com/myview/MainActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.myview.widght.CheckView;
+import com.myview.widght.Circle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +16,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pathdemo);
-
+        setContentView(R.layout.circle);
+        Circle circle = (Circle) findViewById(R.id.test);
+        circle.setOnClickListener(new Circle.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"kanaknkankan",Toast.LENGTH_SHORT).show();
+            }
+        });
 //        this.checkview = (CheckView) findViewById(R.id.checkview);
     }
 
